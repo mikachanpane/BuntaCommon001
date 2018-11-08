4f11a11029f2088b400273573e62cb9353d56188
mikachanpane <kuchanpane1@gmail.com>
2018-11-08 23:08:20 +0900

    first
    f

---
+++ CommonPrj/CommonPrj/DtEx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace CommonPrj
+{
+    //TODO ラムダ式で操作
+    /// <summary>
+    /// データテーブル制御用
+    /// </summary>
+    static class DataExp
+    {
+        /// <summary>
+        /// 行編集
+        /// </summary>
+        /// 
+        /// <returns></returns>
+        private DataRow RowEx(DataRow Row)
+        {
+            //TODO Unit
+            var tst = "";
+
+            //DataR
+            string 
+
+            //カレント行編集結果
+            return Row;
+        }
+    }
+}
---
+++ CommonPrj/CommonPrj/Exports.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonPrj
+{
+
+    /// <summary>
+    /// 出力用パーツクラス
+    /// </summary>
+    static class Exports
+    {
+        /// <summary>
+        /// 出力用整形
+        /// </summary>
+        /// <param name="val">対象</param>
+        /// <returns>結果</returns>
+        private string OutTo(string val, ref string err)
+        {
+            //TODO Unit
+            var tst = "";
+
+            //エラー用
+            string rslt = string.Empty;
+
+            //拡張メソッドを用いて、null許容型で変換をかける
+
+            //変換結果に対して、出力フォーマット
+            tst
+
+            //結果を返す
+            return rslt;
+        }
+
+
+    }
+}
